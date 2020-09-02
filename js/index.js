0845d5902c482900501c5b9ae5ff5b2772196b2c
Donald Poland <polandd@Donalds-MacBook-Pro.local>
2020-09-02 01:02:37 -0400

    Add Javascript for textboxes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,36 @@
-var element = document.getElementById('login');
-element.addEventListener('focus', function() {
-  element.setAttribute('placeholder', '')
-})
+let extension = 'php';
+
+function doLogin() {
+    // TODO: Set up hashing and login with API endpoint.
+}
+
+function saveCookie()
+{
+    // TODO: Save cookie to remind user logged in.
+}
+
+function readCookie() 
+{
+    // TODO: Read possible cookie.
+}
+
+function doLogout() 
+{
+    // TODO: Setup logout functionality.
+}
+
+// Below functions make textboxes smoother to use.
+function clearText(obj, str)
+{
+  if (obj.value == str) 
+  {
+      obj.value = "";
+  }
+}
+function regenerateText(obj, str)
+{
+  if (obj.value == '')
+  {
+      obj.value = str;
+  }
+}
